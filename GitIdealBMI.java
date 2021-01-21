ff48a14f2e551b1942c99f09ecf4ed8f8822cffe
Edgar Hadhyra Julio <edgar.hadhyra@gmail.com>
2021-01-21 13:27:11 +0700

    initial project version

---
+++ GitIdealBMI.java
@@ -0,0 +1,22 @@
+public class GitIdealBMI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Name   : Edgar Hadhyra Julio \n"
+                +          "Age    : 20 years old \n"
+                +          "Gender : Female \n"
+                +          "Height : 167 cm \n"
+                +          "Weight : 55 kg \n"
+                +          "Result : For the information you entered:\n" +
+                           "         Height: 167 centimeters\n" +
+                           "         Weight: 55 kilogram\n" +
+                           "         Your BMI is 20, indicating your weight is in the Normal category.\n"
+                +          "Tips   : It's important to exercise at least 60-90 minutes most days\n" +
+                           "         of the week. Staying hydrated and eat a balanced diet\n" +
+                           "         are equally important.");
+    }
+    
+}
